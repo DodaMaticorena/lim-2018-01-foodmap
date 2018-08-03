67e9d70b6be11ef12b56950a7e3587e114b1ecce
DodaMaticorena <doda.maticorena@gmail.com>
2018-08-03 01:46:55 -0500

    vista splash, cambio a un solo html

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 const firstView = () => {
   document.querySelector('#splash-view').style.display = 'none';
+  document.querySelector('.container').style.display = 'flex';
 }
 window.onload = setTimeout(firstView, 2000);
