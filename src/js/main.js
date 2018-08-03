65ded957635e3694d2e9b433cce3e5fc15c5a240
DodaMaticorena <doda.maticorena@gmail.com>
2018-08-03 16:41:10 -0500

    cambios en el maquetado

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
+// Google maps
 const mapGoogle = {
-  center: new google.maps.LatLng(37.7831,-122.4039),
-  zoom: 12,
+  center: new google.maps.LatLng(-12.046374,-77.042793),
+  zoom: 11,
   mapTypeId: google.maps.MapTypeId.ROADMAP
 };
 
 new google.maps.Map(document.querySelector('#map'), mapGoogle);
+
+// const inputFilter = document.querySelector('#filter-options').addEventListener('click',() => {
+
+// })
